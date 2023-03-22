a27b872e02c7ae857cd9ff1f0b78e4f310dd3e1e
@greninja00 <12deep2001@gmail.com>
2023-03-22 17:38:31 +0530

    completed the updation of scores

--- script.js
+++ script.js
@@ -23,27 +23,50 @@ function playRound(computerSelection, playerSelection)  // to play round and get
     else if (computerSelection =='rock' && playerSelection == 'paper')
     {
         display.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        playerScores++;
     }
     else if (computerSelection =='rock' && playerSelection == 'scissors')
     {
         display.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerScores++;
     }
     else if (computerSelection =='paper' && playerSelection == 'scissors')
     {
         display.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        playerScores++;
     }
     else if (computerSelection =='paper' && playerSelection == 'rock')
     {
         display.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerScores++;
     }
     else if (computerSelection =='scissors' && playerSelection == 'rock')
     {
         display.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        playerScores++;
     }
     else if (computerSelection =='scissors' && playerSelection == 'paper')
     {
         display.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerScores++;
     }
+    scores.textContent = `Your Score is ${playerScores} and Computer score is ${computerScores}`;
+    //updates the scores if we play the game
+
+
+     // Now to reset the game when we reach the final score 
+    if(computerScores == 5)
+    {
+        display.textContent = "Computer wins the game"
+        playerScores = 0;
+        computerScores = 0;
+    }
+    else if(playerScores == 5)
+    {
+        display.textContent = "Player wins the game"
+        computerScores = 0;
+        playerScores = 0;
+    } 
 
 }
 
@@ -53,9 +76,12 @@ const paper = document.querySelector('.Paper');
 const scissors = document.querySelector('.Scissors');  // query selection for dom manipulation
 const display = document.querySelector('.result');
 const scores = document.querySelector('.score-board')
+let playerScores = 0;
+let computerScores = 0;
 
 rock.addEventListener('click',() =>{playRound(getComputerChoice(), 'rock')} );
 paper.addEventListener('click',() =>{playRound(getComputerChoice(), 'paper')} ); // butttons function
 scissors.addEventListener('click',() =>{playRound(getComputerChoice(), 'scissors')} );
+
 
 
